46e601929a5d431f10fc9bca90ca8a1f02982b2f
aidanliddiard <aidanliddiard@gmail.com>
2022-02-04 16:05:21 -0800

    modal finally working

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const form = document.getElementById('input-monster');
 const monstersList = document.querySelector('.monsters');
 const defeatedMonstersSpan = document.getElementById('defeated-monsters');
 const fairyHP = document.getElementById('hp');
+const userAction = document.getElementById('user-actions');
+const monsterAction = document.getElementById('monster-actions');
+const modalClass = document.querySelector('.modal');
 
 let userHP = 10;
 let defeatedMonsters = 0;
@@ -52,6 +55,7 @@ function displayMonsters() {
         const monsterEl = renderMonster(monster);
 
         monsterEl.addEventListener('click', () => {
+            modalClass.classList.add('visible');
             monsterClickHandler(monster);
         });
 
@@ -63,6 +67,8 @@ function displayMonsters() {
 displayMonsters();
 
 function monsterClickHandler(monsterData) {
+    
+    console.log('clicking');
     if (monsterData.hp === 0) return;
     if (userHP === 0) {
         alert('You lost. 😩 Game Over.');
@@ -71,27 +77,37 @@ function monsterClickHandler(monsterData) {
 
     let userRand = Math.ceil(Math.random() * 10);
     if (userRand < 5) {
-        //console.log('i hit' + userRand);
         monsterData.hp--;
-        //console.log(monsterData.hp);
-        alert(`Your spell hit ${monsterData.name}!`);
+    
+        const userHit = new Audio('assets/541477__eminyildirim__magic-earth-spell-impact-punch.wav');
+        userHit.play();
+        userAction.textContent = (`Your spell hit ${monsterData.name}!`);
     }
     else {
-        alert(`Your spell missed ${monsterData.name}!`);
+        const userMiss = new Audio('assets/577965__colorscrimsontears__rpgpowerup.wav');
+        userMiss.play();
+        userAction.textContent = (`${monsterData.name} deflected your spell!`);
     }
     displayMonsters();
   
-    let monsterRand = Math.ceil(Math.random() * 10);
-    if (monsterRand > 6) {
-        //console.log('they hit' + monsterRand);
-        userHP--;
-        //console.log(userHP);
-        alert(`${monsterData.name} hit you!`);
-    }
-    else {
-        alert(`${monsterData.name} missed you!`);
-    }
-    fairyHP.textContent = userHP;
+    
+    setTimeout(() => {
+        let monsterRand = Math.ceil(Math.random() * 10);
+        if (monsterRand > 6) {
+            userHP--;
+      
+            const monsterHit = new Audio('/assets/541478__eminyildirim__magic-fire-spell-impact-punch.wav');
+            monsterHit.play();
+            monsterAction.textContent = (`${monsterData.name} got past your forcefield spell!`);
+        }
+        else {
+            const monsterMiss = new Audio('assets/253176__suntemple__retro-spell-sfx.wav');
+            monsterMiss.play();
+            monsterAction.textContent = (`${monsterData.name} missed you! Your forcefield spell worked.`);
+        }
+        fairyHP.textContent = userHP;
+    }, 2000
+    );
 
     if (monsterData.hp === 0) {
         defeatedMonsters++;
